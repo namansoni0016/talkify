dbefe8e84b0ce0e51974e003e10b62c3d6b3ea17
Naman Soni <namantsoni@gmail.com>
2025-01-03 22:50:21 +0530

    Signup backend functionality

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 
 const app = express();
+
+//Middlewares
+app.use(express.json());
+
+//Routes
+app.use("/api/auth", authRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
