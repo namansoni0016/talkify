7ed45597f1811c0d675f5eb00332cd2116bfc524
Naman Soni <namantsoni@gmail.com>
2025-01-04 18:59:51 +0530

    Message backend functionality

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 dotenv.config();
 
 const app = express();
@@ -13,6 +14,7 @@ app.use(cookieParser());
 
 //Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
