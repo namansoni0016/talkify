f6b424e74e573cddb096a6897f164928b7aa46d1
Naman Soni <namantsoni@gmail.com>
2025-01-06 19:21:50 +0530

    Complete Sign Up page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import SettingsPage from './pages/SettingsPage';
 import ProfilePage from './pages/ProfilePage';
 import { useAuthStore } from './store/useAuthStore';
 import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
@@ -23,7 +24,7 @@ const App = () => {
     )
   }
   return (
-    <div className='text-red-500'>
+    <div>
       <Navbar />
       <Routes>
         <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />} />
@@ -32,6 +33,7 @@ const App = () => {
         <Route path='/settings' element={<SettingsPage />} />
         <Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
+      <Toaster />
     </div>
   );
 }
