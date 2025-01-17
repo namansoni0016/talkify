665436d60b5b87078f3cb32628865bcbfc29d305
Naman Soni <namantsoni@gmail.com>
2025-01-17 10:58:51 +0530

    Final Deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 dotenv.config();
+
+const __dirname = path.resolve();
 
 //Middlewares
 app.use(express.json({ limit: '10mb' }));
@@ -19,6 +22,13 @@ app.use(cors({
 //Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV==="production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
